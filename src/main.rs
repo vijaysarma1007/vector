852e16e9ba7d24bdebfa48d6fd596409749e0e36
vijaysarma1007 <svmsarma53@gmail.com>
2026-03-21 13:49:11 +0530

    added mutable reference

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,55 @@ fn main() {
     let pepperoni = String::from("Pepperoni");
     let mushroom = String::from("Mushroom");
     let sausage = String::from("Sausage");
-    let pizza_topping = vec![pepperoni, mushroom, sausage];
+    let mut pizza_topping = vec![pepperoni, mushroom, sausage];
+    pizza_topping[1] = String::from("Olives");
+    println!("{pizza_topping:?}");
 
-    let value = &pizza_topping[2];
+    let target_topiing = &mut pizza_topping[2];
+    target_topiing.push_str(" and Meatballs");
+    let another_topping = &pizza_topping[2];
+    println!("{pizza_topping:?}");
+    // let mut delicous_toppings = pizza_topping;
+
+    // let topping_reference = &delicous_toppings[1];
+
+    // delicous_toppings.push(String::from("Olives"));
+
+    //println!("The topping is {topping_reference}");
+
+    //let value = &pizza_topping[2];
 
     let pizza_slice = &pizza_diameters[1..3];
     println!("{pizza_slice:?}");
 
-    let option = pizza_topping.get(2);
-    match option {
-        Some(topping) => println!("The topping is {topping}"),
-        None => println!("No value at the index position"),
-    }
+    // let option = pizza_topping.get(2);
+    // match option {
+    //     Some(topping) => println!("The topping is {topping}"),
+    //     None => println!("No value at the index position"),
+    // }
+
+    let mut seasons: Vec<&str> = Vec::with_capacity(4);
+    println!(
+        "Length : {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
+
+    seasons.push("Summer");
+    seasons.push("Fall");
+    seasons.push("Spring");
+    seasons.push("Winter");
+
+    println!(
+        "Length : {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
+
+    seasons.push("Rainy");
+        println!(
+        "Length : {}, Capacity: {}",
+        seasons.len(),
+        seasons.capacity()
+    );
 }
